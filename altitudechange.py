4d8ffad31cd4fafff5241e68768f7a1686119a33
louloubrouwers <louloubrounch@gmail.com>
2025-06-02 15:39:16 +0200

    j change

--- altitudechange.py
+++ altitudechange.py
@@ -57,28 +57,28 @@ def TimeUnder5m(srt, t):
 
 '''---------explanation of naming convention for speeds ---------------------
 Vd = velocity of debris (starting in circular orbit)
-Vm = Velocity of spacecraft starting in Vd - relative velocity 
+Vma = Velocity of spacecraft starting in Vd - relative velocity 
 D_Vbd = The burn delta V applied to the debris after each burn 
 D_Vbdtot = total delta V applied to debris after X rendezvous
 D_Vbm = change of delta V applied to ourselves after a burn 
-D_Vm = delta V for next rendez vous
+D_Vma = delta V for next rendez vous
 D_Vtot =  total delta V required, taking into account delta V for next rdv and delta V applied during rdv
 V_trans = velocity for transfer to next debris'''
 
 
 
 #find delta V required to meet after two orbits
-def twoorbit(Vd, Vm):                    
+def twoorbit(Vd, Vma):                    
     ad = 1/(2/((debris_array[i,0]*1000)+6371000)-Vd**2/mu)                          #calculate new semi major axis of debris
     Td = 2*np.pi*np.sqrt(ad**3/mu)                          #calculate new orbital period of debris
-    am = 1/(2/((debris_array[i,0]*1000)+6371000)-Vm**2/mu)                          #calculate new semi major axis of spacecraft
+    am = 1/(2/((debris_array[i,0]*1000)+6371000)-Vma**2/mu)                          #calculate new semi major axis of spacecraft
     Tm = 2*np.pi*np.sqrt(am**3/mu)                          #calculate new orbital period of spacecraft
     T_desired = 2*Td-Tm                                     #Desired new orbital period: debris does two periods in same orbit while we change orbit
     a_desired = ((T_desired/(2*np.pi))**(2/3))*mu**(1/3)    #new semi maor axis of the spacecraft
     V_desired = np.sqrt(mu*(2/((debris_array[i,0]*1000)+6371000)-1/a_desired))      #desired velocity at apogee to meet on time
-    dvm = Vm-V_desired                                      #delta V required
+    dVma = V_desired - Vma                                      #delta V required
 
-    return dvm
+    return dVma
 
 def dvbtot(h):
     # h in km
@@ -94,7 +94,7 @@ def period (ha, va):
     sma = 1/(2/(ha+6371000)-va**2/mu)
     return 2 * np.pi * np.sqrt(sma**3 / mu)  # Period in seconds
 
-def phi_change(h,t,phi):
+def phi_change(h,t):
     T = 2*np.pi((((h+6371)*1000)**3)/mu)
     rest = t%T
     d_phi = rest/T*360
@@ -116,6 +116,7 @@ phi = [0, 15, 25, 35, 45, 55, 65, 75, 85, 95]  # Phase angle of debris in degree
 minimum_dv = 10000000
 sequence = []
 debris_array = np.column_stack((h, md, phi))  # shape (10, 3)
+
 indices = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]  # Indices for debris
 indicess = []
 for perm in itertools.permutations(indices):
@@ -126,15 +127,13 @@ for k in range(len(indicess)):
     time = 0 
     M = 1494.3 
     Mi=M
-    Isp = 342
-    g0 = 9.80665
     Sro = -5
     Vros = np.linspace(0, 3, 100)   # Wider range for Vro
     t = np.linspace(0.1, 15, 150)     # Time vector (start from 0.1 to avoid log(0))
     T = 465  
-    Vd = np.sqrt(mu/((debris_array[0,0]+6371)*1000))
-    Vro = OptVro(t, T, eta, debris_array[0,1], M, Sro, Vros, Isp) 
-    Vm = Vd - Vro
+    Vd = np.sqrt(mu/((debris_array[indicess[k][0],0]+6371)*1000))
+    Vro = OptVro(t, T, eta, debris_array[indicess[k][0],1], M, Sro, Vros, Isp) 
+    Vma = Vd - Vro
     D_Vtot = np.abs(Vro)
     M = M/(np.exp(np.abs(Vro)/(Isp*g0)))
     bs = 0
@@ -142,9 +141,6 @@ for k in range(len(indicess)):
         i = indicess[k][j]
         D_Vbdtot= 0
         Vd = np.sqrt(mu/((debris_array[i,0]+6371)*1000))                                            #debris velocity in circular orbit
-        
-        # if i == 0:
-        #     print (SmaandE(Vm))
         b = 0                                                                   
         while D_Vbdtot <= dvbtot(debris_array[i,0]):                                                   #stop the while loop when delta V applied to debris is enough to deorbit
             b +=1                                                                  #number of rdv per debris
@@ -156,50 +152,50 @@ for k in range(len(indicess)):
             Vd = Vd - D_Vbd  
             D_Vbdtot += D_Vbd                                                      #update total debris velocity change                                              
             D_Vbm = Isp*g0*np.log(M/(M-t_under_5*T/(Isp*g0)))                      #Delta V applied to ourselves during burn
-            Vm = Vm + D_Vbm                                                        #update our spacecraft velocity
+            Vma = Vma + D_Vbm                                                        #update our spacecraft velocity
             M = M/(np.exp(np.abs(D_Vbm)/(Isp*g0)))                                         #update our mass after momentum transfer   
                                              #update time after rdv
             #check if it was the last burn 
             if D_Vbdtot >= dvbtot(debris_array[i,0]):
-                time += period(debris_array[i,0], Vm)/2
+                time += period(debris_array[i,0], Vma)/2
                 break                        
-            time += period(debris_array[i,0], Vm)              
-            D_Vm = twoorbit(Vd,Vm)                                                 #see function explanation above
-            Vm -= D_Vm                                                             #update velocity
-            M = M/(np.exp(np.abs(D_Vm)/(Isp*g0)))  
-            time += period(debris_array[i,0], Vm)                                        #update mass
+            time += period(debris_array[i,0], Vma)              
+            D_Vma = twoorbit(Vd,Vma)                                                 #see function explanation above
+            Vma += D_Vma                                                             #update velocity
+            M = M/(np.exp(np.abs(D_Vma)/(Isp*g0)))  
+            time += period(debris_array[i,0], Vma)                                        #update mass
             # Vro = OptVro(t, T, eta, debris_array[i,1], M, Sro, Vros, Isp)                      #update Vro
             Vro = 2.5
-            D_V_corr2 = (Vd-Vm) - Vro                                              #correction to achieve desired relative velocity
-            Vm += D_V_corr2 
-            M = M/(np.exp(np.abs(D_V_corr2)/(Isp*g0)))                             #update Vm again to prepare for new momentum transfer
-            D_Vtot = D_Vtot + D_Vm + D_Vbm + np.abs(D_V_corr2)
+            D_V_corr2 = (Vd-Vma) - Vro                                              #correction to achieve desired relative velocity
+            Vma += D_V_corr2 
+            M = M/(np.exp(np.abs(D_V_corr2)/(Isp*g0)))                             #update Vma again to prepare for new momentum transfer
+            D_Vtot = D_Vtot + np.abs(D_Vma) + np.abs(D_Vbm) + np.abs(D_V_corr2)
             
         bs += b
 
-        #print(f'number of rdv for debris{i+1}: {b}')
-
-        if i < 9:           
+        if j < 9:           
             # Vro = OptVro(t, T, eta, debris_array[i,1], M, Sro, Vros, Isp)                                                             #not take extra transfer into account for last debris (EOL)
             Vro = 2.5
-            a1 = 1/(2/(debris_array[i, 0]*1000+6371000)-Vm**2/mu)
-            hp = 2*a1-(debris_array[i, 0]*1000+6371000)-6371000 #periapsis of our orbit
-            a2 = (hp + 6371000*2 + debris_array[i+1, 0]*1000)/2  #semi major axis of next debris
-            vp1 = np.sqrt(mu*(2/(hp+6371000)-1/(a1)))
-            vp2 = np.sqrt(mu*(2/(hp+6371000)-1/(a2)))
+            a1 = 1/(2/(debris_array[i, 0]*1000+6371000)-Vma**2/mu) #m
+            hp = 2*a1-(debris_array[i, 0]*1000+6371000)-6371000 #periapsis of our orbit m
+            a2 = (hp + 6371000*2 + debris_array[i+1, 0]*1000)/2  #semi major axis of next debris m 
+            vp1 = np.sqrt(mu*(2/(hp+6371000)-1/(a1))) # m/s 
+            vp2 = np.sqrt(mu*(2/(hp+6371000)-1/(a2))) # m/s
             D_V_Peri = vp2 - vp1
-            Vm = np.sqrt(mu*(2/((debris_array[i+1,0]+6371)*1000)-1/(a2)))   #update Vm for next rendezvous
+            Vma = np.sqrt(mu*(2/((debris_array[i+1,0]+6371)*1000)-1/(a2)))   #find Vma at apogee m/s
             D_Vtot += np.abs(D_V_Peri)
             M = M/(np.exp(np.abs(D_V_Peri)/(Isp*g0)))  #update mass after transfer velocity
-            time += period(debris_array[i+1,0], Vm)/2  #update time after transfer velocity
+            time += period(debris_array[i+1,0], Vma)/2  #update time after transfer velocity
+
             for w in range(j+1,10):
-                i = indicess[w][j]
-                phi_copy[w] = phi_copy[w] + phi_change(debris_array[w,0],time)
+                i = indicess[k][w]
+                phi_copy[i] = (phi_copy[i] + phi_change(debris_array[i,0],time))%360
+                
             time = 0
             while phi_copy[i+1] > np.abs(catchup_phi(a2, debris_array[i+1,0])):
                 delta_phi = catchup_phi(a2, debris_array[i+1,0])  #calculate phase angle change for next rendezvous
                 phi_copy[i+1]= (phi_copy[i+1] + delta_phi)  #update phase angle for next rendezvous
-                time += period(debris_array[i+1,0], Vm)  #update time after phase angle change
+                time += period(debris_array[i+1,0], Vma)  #update time after phase angle change
             Vc = np.sqrt(mu/((debris_array[i+1,0]+6371)*1000))  #velocity of debris in circular orbit
             max_delta_phi = (period(debris_array[i+1,0], Vc) - period(debris_array[i+1,0], Vc-5))/(period(debris_array[i+1,0], Vc))*360  #(0.7)maximum phase angle change for next rendezvous
             N = 1
@@ -208,15 +204,15 @@ for k in range(len(indicess)):
             delta_phi = -phi_copy[i+1]/N  #calculate phase angle change for next rendezvous
             T_adr = delta_phi*period(debris_array[i+1,0], Vc)/360+ period(debris_array[i+1,0], Vc)
             a_adr = ((T_adr/(2*np.pi))**(2/3))*mu**(1/3)  #semi major axis of our spacecraft after phase angle change
-            D_V_trans = np.sqrt(mu*(2/((debris_array[i+1,0]+6371)*1000)-1/a_adr))-Vm  #update Vm for next rendezvous
+            D_V_trans = np.sqrt(mu*(2/((debris_array[i+1,0]+6371)*1000)-1/a_adr))-Vma  #update Vma for next rendezvous
             D_Vtot += np.abs(D_V_trans)
-            Vm += D_V_trans
+            Vma += D_V_trans
             M = M/(np.exp(np.abs(D_V_trans)/(Isp*g0)))  #update mass after transfer velocity
-            time += N*period(debris_array[i+1,0], Vm)  #update time after transfer velocity
+            time += N*period(debris_array[i+1,0], Vma)  #update time after transfer velocity
             
 
-            D_V_corr = np.sqrt(mu/((debris_array[i+1,0] +6371)*1000)) -Vm - Vro
-            Vm += D_V_corr              #add transfer velocity to rdv with new debris 
+            D_V_corr = np.sqrt(mu/((debris_array[i+1,0] +6371)*1000)) -Vma - Vro
+            Vma += D_V_corr              #add transfer velocity to rdv with new debris 
             D_Vtot = D_Vtot + np.abs(D_V_corr)
             M = M/(np.exp(np.abs(D_V_corr)/(Isp*g0)))  
         
